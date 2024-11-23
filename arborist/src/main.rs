65d5dd019d8082569fe004e79bae9f45df8b050f
edgerunnergit <edgerunnergit@riseup.net>
2024-11-24 02:03:42 +0530

    added sample data

--- arborist/src/main.rs
+++ arborist/src/main.rs
@@ -24,13 +24,15 @@ async fn main() -> anyhow::Result<()> {
     let scan_config = DirScanConfig::new(args.path);
     let mut scan_result = scan_config.scan_dir().await?;
 
-    println!("{:#?}", scan_result);
+    //println!("{:#?}", scan_result);
 
     // Generate summaries for each folder
     for folder in &mut scan_result.file_metadata_list {
-        println!("generating summary for: {:?}", folder);
+        println!("generating summary for: {:#?}", folder);
         let summary = generate_file_summary("gemma2:2b", folder).await?;
-        println!("Folder Summary: {}", summary);
+        println!("--------------------------------------------");
+        println!("File Summary: {}", summary);
+        println!("--------------------------------------------");
         folder.summary = summary;
     }
 
