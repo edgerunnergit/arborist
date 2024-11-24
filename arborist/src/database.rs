95fc687f4c35e2d28f2188dfb62944e61a08ba75
edgerunnergit <edgerunnergit@riseup.net>
2024-11-24 23:34:02 +0530

    v0 done

---
+++ arborist/src/database.rs
@@ -0,0 +1,209 @@
+use crate::file_management::FileMetadata;
+use anyhow::Result;
+use fastembed::{EmbeddingModel, InitOptions, SparseEmbedding, SparseTextEmbedding, TextEmbedding};
+use qdrant_client::qdrant::{
+    Condition, CreateCollectionBuilder, Distance, Filter, PointStruct, QueryPointsBuilder,
+    SearchParamsBuilder, SparseVectorParamsBuilder, SparseVectorsConfigBuilder, UpsertPoints,
+    Value, VectorParamsBuilder, VectorsConfigBuilder,
+};
+use qdrant_client::{Payload, Qdrant};
+use std::collections::HashMap;
+use text_splitter::{ChunkConfig, TextSplitter};
+use tokenizers::Tokenizer;
+use uuid::Uuid;
+
+pub async fn create_hybrid_collection(client: &Qdrant, collection_name: &str) -> Result<()> {
+    // Check if the collection already exists
+    if client.collection_exists(collection_name).await? {
+        println!(
+            "Collection '{}' already exists. Skipping creation.",
+            collection_name
+        );
+        return Ok(());
+    }
+    // Configure sparse vectors using builder
+    let mut sparse_vector_config = SparseVectorsConfigBuilder::default();
+    sparse_vector_config.add_named_vector_params("splade", SparseVectorParamsBuilder::default());
+
+    // Configure dense vectors using builder
+    let mut dense_vector_config = VectorsConfigBuilder::default();
+    dense_vector_config
+        .add_named_vector_params("novum", VectorParamsBuilder::new(768, Distance::Cosine));
+
+    // Create collection using builders
+    client
+        .create_collection(
+            CreateCollectionBuilder::new(collection_name)
+                .sparse_vectors_config(sparse_vector_config)
+                .vectors_config(dense_vector_config),
+        )
+        .await?;
+
+    println!("collection created");
+
+    Ok(())
+}
+
+fn chunk_string(
+    input: &str,
+    tokenizer_name: &str,
+    max_tokens: std::ops::Range<usize>,
+) -> Vec<String> {
+    // Initialize the tokenizer
+    let tokenizer =
+        Tokenizer::from_pretrained(tokenizer_name, None).expect("Failed to load tokenizer");
+
+    // Create the TextSplitter with ChunkConfig
+    let splitter = TextSplitter::new(ChunkConfig::new(max_tokens).with_sizer(tokenizer));
+
+    // Chunk the input string and collect the results
+    splitter
+        .chunks(input)
+        //.into_iter()
+        .map(|chunk| chunk.to_string())
+        .collect()
+}
+
+/// Generate both sparse and dense embeddings for a list of summaries
+async fn generate_embeddings(
+    summary: String,
+    model: &TextEmbedding,
+    sparse_model: &SparseTextEmbedding,
+) -> Result<(Vec<Vec<f32>>, Vec<SparseEmbedding>)> {
+    // Generate dense embeddings
+    let summary_chunks = chunk_string(&summary, "bert-base-cased", 20..40);
+    let dense_embeddings = model.embed(summary_chunks, None)?;
+
+    // Generate sparse embeddings
+    let sparse_embeddings = sparse_model.embed([summary].to_vec(), None)?;
+
+    Ok((dense_embeddings, sparse_embeddings))
+}
+
+pub async fn process_and_upload_files(
+    client: &Qdrant,
+    file_metadata_list: &Vec<FileMetadata>,
+) -> Result<()> {
+    let model = TextEmbedding::try_new(
+        InitOptions::new(EmbeddingModel::NomicEmbedTextV15).with_show_download_progress(true),
+    )?;
+    let sparse_model = SparseTextEmbedding::try_new(Default::default())?;
+
+    let mut points: Vec<PointStruct> = Vec::new();
+
+    for file in file_metadata_list {
+        // Check if the file path already exists in the collection
+        let query_result = client
+            .query(
+                QueryPointsBuilder::new("file_data")
+                    .filter(Filter::must([Condition::matches(
+                        "file_path",
+                        file.path.clone(),
+                    )]))
+                    .limit(1),
+            )
+            .await;
+
+        if let Ok(response) = query_result {
+            if !response.result.is_empty() {
+                println!("File path '{}' already exists. Skipping.", file.path);
+                continue;
+            }
+        } else {
+            eprintln!(
+                "Error querying file path '{}': {:?}. Skipping.",
+                file.path, query_result
+            );
+            continue;
+        }
+        let summary = file.summary.clone();
+
+        // Generate both dense and sparse embeddings
+        let (dense_embeddings, sparse_embeddings) =
+            match generate_embeddings(summary, &model, &sparse_model).await {
+                Ok(embeddings) => embeddings,
+                Err(err) => {
+                    eprintln!("Error generating embeddings for {}: {}", file.name, err);
+                    continue; // Skip to the next file if embeddings fail
+                }
+            };
+
+        // Prepare payload
+        let mut payload = Payload::new();
+        payload.insert("file_name", Value::from(file.name.clone()));
+        payload.insert("file_path", Value::from(file.path.clone()));
+        payload.insert("file_size", Value::from(file.size as i64));
+
+        if let (Some(dense_embedding), Some(_sparse_embedding)) =
+            (dense_embeddings.first(), sparse_embeddings.first())
+        {
+            let mut vectors_map: HashMap<String, Vec<f32>> = HashMap::new();
+            vectors_map.insert("novum".to_string(), dense_embedding.clone());
+
+            // TODO: fix adding sparse vectors
+            //let sparse_values: Vec<f32> = sparse_embedding.values.to_vec(); // Extract values
+            //vectors_map.insert("splade".to_string(), sparse_values);
+
+            let uuid = Uuid::new_v4();
+
+            let point = PointStruct::new(uuid.to_string(), vectors_map, payload.clone());
+            points.push(point);
+        } else {
+            eprintln!(
+                "Error: Could not generate embeddings for file: {}",
+                file.name
+            );
+        }
+    }
+    // Now upsert the collected points
+    match client
+        .upsert_points(UpsertPoints {
+            collection_name: "file_data".to_string(),
+            wait: Some(true),
+            points,
+            ..Default::default() // Use default values for other fields
+        })
+        .await
+    {
+        Ok(_) => println!("Points upserted successfully!"),
+        Err(err) => eprintln!("Error upserting points: {}", err),
+    };
+
+    Ok(())
+}
+
+/// Query the database using a vector and print matching file paths
+pub async fn query_and_print_file_paths(
+    client: &Qdrant,
+    collection_name: &str,
+    query_vector: Vec<f32>,
+    limit: usize,
+    use_sparse: bool, // Option to toggle between dense and sparse vector search
+) -> anyhow::Result<()> {
+    // Specify the vector type to use in the query
+    let vector_type = if use_sparse { "splade" } else { "novum" };
+
+    // Perform the query
+    let query_result = client
+        .query(
+            QueryPointsBuilder::new(collection_name)
+                .query(query_vector)
+                .using(vector_type)
+                .limit(limit as u64)
+                .with_payload(true)
+                .params(SearchParamsBuilder::default().hnsw_ef(128).exact(false)), // Configure search parameters
+        )
+        .await?;
+
+    // Extract and print matching file paths
+    for point in query_result.result {
+        //if let Some(payload) = point.payload {
+        //    if let Some(file_path) = payload.get("file_path").and_then(Value::as_str) {
+        //        println!("File Path: {}", file_path);
+        //    }
+        //}
+        println!("Result for Query: {:#?}", point);
+    }
+
+    Ok(())
+}
--- arborist/src/main.rs
+++ arborist/src/main.rs
@@ -1,5 +1,11 @@
+use anyhow::Ok;
 use clap::Parser;
+use fastembed::{EmbeddingModel, InitOptions, SparseTextEmbedding, TextEmbedding};
+use qdrant_client::qdrant::ScrollPointsBuilder;
+use qdrant_client::Qdrant;
 use summary::generate_file_summary;
+use text_splitter::{ChunkConfig, TextSplitter};
+use tokenizers::Tokenizer;
 use utils::DirScanConfig;
 
 mod database;
@@ -15,26 +21,66 @@ mod utils;
 struct Args {
     #[clap(short, long)]
     path: String,
+    #[clap(short, long)]
+    query: String,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
+    let client = Qdrant::from_url("http://localhost:6334").build()?;
+    let model = TextEmbedding::try_new(
+        InitOptions::new(EmbeddingModel::NomicEmbedTextV15).with_show_download_progress(true),
+    )?;
+    let sparse_model = SparseTextEmbedding::try_new(Default::default())?;
+
+    database::create_hybrid_collection(&client, "file_data").await?;
+
     let scan_config = DirScanConfig::new(args.path);
     let mut scan_result = scan_config.scan_dir().await?;
 
     //println!("{:#?}", scan_result);
+    let tokenizer = Tokenizer::from_pretrained("bert-base-cased", None).unwrap();
+    let max_tokens = 20..40;
+    let splitter = TextSplitter::new(ChunkConfig::new(max_tokens).with_sizer(tokenizer));
 
-    // Generate summaries for each folder
-    for folder in &mut scan_result.file_metadata_list {
-        println!("generating summary for: {:#?}", folder);
-        let summary = generate_file_summary("gemma2:2b", folder).await?;
+    //Generate summaries for each folder
+    for file in &mut scan_result.file_metadata_list {
+        println!("generating summary for: {:#?}", file);
+        let summary = generate_file_summary("gemma2:2b", file).await?;
         println!("--------------------------------------------");
         println!("File Summary: {}", summary);
         println!("--------------------------------------------");
-        folder.summary = summary;
+        file.summary = summary.clone();
+
+        let chunks = splitter.chunks(&summary);
+        for chunk in chunks {
+            println!("chunk here: {}", chunk)
+        }
+
+        //let chunks = splitter.chunks(summary;
+        let summary_vec = vec![summary];
+        let embeddings = model.embed(summary_vec.clone(), None)?;
+        let _sparse_embeddings = sparse_model.embed(summary_vec, None)?;
+
+        println!("Embeddings length: {}", embeddings.len()); // -> Embeddings length: 4
+        println!("Embedding dimension: {}", embeddings[0].len()); // -> Embedding dimension: 384
     }
+
+    let query_vector = model.embed([args.query].to_vec(), None)?[0].clone(); // Example query vector
+    let collection_name = "file_data";
+
+    // Query and print file paths using dense vector matching
+    database::query_and_print_file_paths(&client, collection_name, query_vector, 5, false).await?;
+
+    //database::process_and_upload_files(&client, &scan_result.file_metadata_list).await?;
+
+    let a = client
+        .scroll(ScrollPointsBuilder::new("file_data").limit(10))
+        .await?;
+
+    //println!("All elems: {:#?}", a);
 
     Ok(())
 }
